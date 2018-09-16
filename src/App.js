a636259298a20e6a83d392d5e9266995521e099f
alayor <alayor3@gmail.com>
2018-09-16 16:24:25 -0600

    Improve order view.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p className="App-intro">
-
-        </p>
+        <div className="OrderNo">#23</div>
+        <div OrderDetails>
+          Jamón (2) <br />
+          Lomo (1) <br />
+          Especial (1) <br />
+          Refrescos (4) <br />
+        </div>
       </div>
     );
   }
