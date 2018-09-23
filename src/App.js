b537ccf2452bd4fbd2260fc9f0aa625836f29a62
alayor <alayor3@gmail.com>
2018-09-23 07:15:38 -0600

    Add socket listener.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
 import './App.css'
+import config from './config'
 import * as orderService from './services/orders'
+import io from 'socket.io-client'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      orders: [{
-        id: 0,
-        jamonQuantity: 0,
-        lomoQuantity: 0,
-        especialQuantity: 0,
-        refrescosQuantity: 0,
-      }],
-      currentOrderId: 0
+      orders: [
+        {
+          id: 0,
+          jamonQuantity: 0,
+          lomoQuantity: 0,
+          especialQuantity: 0,
+          refrescosQuantity: 0,
+        },
+      ],
+      currentOrderId: 0,
     }
+    this.socket = {}
   }
 
   async componentDidMount() {
+    this.socket = io(config.API_URL)
+    this.socket.emit('subscribe_for_order_placements', config.mocks.store_location)
+    this.socket.on('placed_order', data => {
+      this.setState({
+        orders: [...this.state.orders, data.order],
+      })
+    })
+
     const response = await orderService.listOrders()
     if (response.data && response.data.length) {
       this.setState({ orders: response.data })
@@ -29,13 +42,17 @@ class App extends Component {
       <div className="App">
         <div className="OrderNo">#{this.state.orders[this.state.orders.length - 1].id}</div>
         <div className="OrderDetails">
-          * Jamón - {this.state.orders[this.state.orders.length - 1].jamonQuantity} <br />
-          * Lomo - {this.state.orders[this.state.orders.length - 1].lomoQuantity} <br />
-          * Especial - {this.state.orders[this.state.orders.length - 1].especialQuantity} <br />
-          * Refrescos - {this.state.orders[this.state.orders.length - 1].refrescosQuantity} <br />
+          * Jamón - {this.state.orders[this.state.orders.length - 1].jamonQuantity} <br />* Lomo -{' '}
+          {this.state.orders[this.state.orders.length - 1].lomoQuantity} <br />* Especial -{' '}
+          {this.state.orders[this.state.orders.length - 1].especialQuantity} <br />* Refrescos -{' '}
+          {this.state.orders[this.state.orders.length - 1].refrescosQuantity} <br />
         </div>
       </div>
     )
+  }
+
+  componentWillUnmount() {
+    this.socket.disconnect()
   }
 }
 
--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 module.exports = {
   get API_URL() {
-    debugger
     return window.location.href === 'https://tortasmundo-orders-display.herokuapp.com/'
       ? `https://tortasmundo-kitchen-api.herokuapp.com` : 'http://localhost:4000'
   },
   mocks: {
-    delivererLocation: {
+    store_location: {
       latitude: '28.1867348',
       longitude: '-105.4608849'
     }
