f7cff407cc5dd219c273855dfd0c12cba52b6c80
alayor <alayor3@gmail.com>
2018-09-23 08:48:49 -0600

    Navigate between orders.

--- src/App.js
+++ src/App.js
@@ -29,23 +29,43 @@ class App extends Component {
       this.setState({
         orders: [...this.state.orders, order],
       })
+      this.setState({
+        currentOrderId: this.state.orders.length - 1
+      })
     })
 
     const response = await orderService.listOrders()
     if (response.data && response.data.length) {
       this.setState({ orders: response.data })
+      this.setState({
+        currentOrderId: this.state.orders.length - 1
+      })
     }
+  }
+
+  nextOrder = (e) => {
+    e.preventDefault();
+    this.setState({
+      currentOrderId: Math.min(this.state.currentOrderId + 1, this.state.orders.length - 1)
+    })
+  }
+
+  previousOrder = (e) => {
+    e.preventDefault();
+    this.setState({
+      currentOrderId: Math.max(this.state.currentOrderId - 1, 0)
+    })
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="OrderNo">#{this.state.orders[this.state.orders.length - 1].id}</div>
+      <div className="App" onClick={this.previousOrder} onContextMenu={this.nextOrder}>
+        <div className="OrderNo">#{this.state.orders[this.state.currentOrderId].id}</div>
         <div className="OrderDetails">
-          * Jamón - {this.state.orders[this.state.orders.length - 1].jamonQuantity} <br />* Lomo -{' '}
-          {this.state.orders[this.state.orders.length - 1].lomoQuantity} <br />* Especial -{' '}
-          {this.state.orders[this.state.orders.length - 1].especialQuantity} <br />* Refrescos -{' '}
-          {this.state.orders[this.state.orders.length - 1].refrescosQuantity} <br />
+          * Jamón - {this.state.orders[this.state.currentOrderId].jamonQuantity} <br />* Lomo -{' '}
+          {this.state.orders[this.state.currentOrderId].lomoQuantity} <br />* Especial -{' '}
+          {this.state.orders[this.state.currentOrderId].especialQuantity} <br />* Refrescos -{' '}
+          {this.state.orders[this.state.currentOrderId].refrescosQuantity} <br />
         </div>
       </div>
     )
