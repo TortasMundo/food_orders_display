62c01674bfe9b84228dc2c69d88c99caf6d0bdf9
Alonso A. Ortega <alayor3@gmail.com>
2018-11-03 13:37:07 -0700

    Fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   get API_URL() {
     return window.location.href === 'https://tortasmundo-orders-display.herokuapp.com/'
-      ? `tortasmundo-kitchen-api-1927554484.us-east-2.elb.amazonaws.com` : 'http://localhost:4000'
+      ? `http://tortasmundo-kitchen-api-1927554484.us-east-2.elb.amazonaws.com` : 'http://localhost:4000'
   },
   mocks: {
     store_location: {
