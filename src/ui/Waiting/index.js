925681957cd8ea0e696a3aded9ffc2f33a0beab0
Alonso A. Ortega <alayor3@gmail.com>
2018-11-03 11:38:32 -0700

    Tortas Mundo

--- src/ui/Waiting/index.js
+++ src/ui/Waiting/index.js
@@ -4,7 +4,7 @@ import { styles } from './styles'
 export const Waiting = (props) => {
   return (
     <div style={styles.text}>
-     <img src="logo-small.png" alt="Girl in a jacket" />
+     <img src="logo-small.png" alt="Tortas Mundo" />
      <div>Esperando ordenes...</div>
     </div>)
 }
