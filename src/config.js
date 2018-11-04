eb404c26935e9fc69284bbdd6b058b243dd2045a
Alonso A. Ortega <alayor3@gmail.com>
2018-11-04 09:22:54 -0700

    Change api url.

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     if (window.location.href === 'https://tortasmundo-orders-display.herokuapp.com/') {
       return `https://tortasmundo-kitchen-api.herokuapp.com`
     }
-    if (window.location.href === 'http://tortasmundo-kitchen-web-ui-1691666950.us-east-2.elb.amazonaws.com/') {
+    if (window.location.href === 'http://tortasmundo-kitchen-web-ui-129681185.us-east-2.elb.amazonaws.com/') {
       return 'http://tortasmundo-kitchen-api-1604690468.us-east-2.elb.amazonaws.com'
     }
     return  'http://localhost:4000'
