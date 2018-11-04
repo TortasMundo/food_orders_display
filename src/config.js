6715a704da135032b3c514eee110a9fc69de2f2a
Alonso A. Ortega <alayor3@gmail.com>
2018-11-03 21:12:10 -0700

    Use aws kitchen api service.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 module.exports = {
   get API_URL() {
-    return window.location.href === 'https://tortasmundo-orders-display.herokuapp.com/'
-      ? `https://tortasmundo-kitchen-api.herokuapp.com` : 'http://localhost:4000'
+    if (window.location.href === 'https://tortasmundo-orders-display.herokuapp.com/') {
+      return `https://tortasmundo-kitchen-api.herokuapp.com`
+    }
+    if (window.location.href === 'http://tortasmundo-kitchen-web-ui-1691666950.us-east-2.elb.amazonaws.com/') {
+      return 'tortasmundo-kitchen-api-1927554484.us-east-2.elb.amazonaws.com'
+    }
+    return  'http://localhost:4000'
   },
   mocks: {
     store_location: {
