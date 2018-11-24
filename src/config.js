7e686578780e6316f3bd6a30f61da0729f342de2
Alonso A. Ortega <alayor3@gmail.com>
2018-11-24 15:55:57 -0700

    Add ec2 endpoint.

--- src/config.js
+++ src/config.js
@@ -6,6 +6,9 @@ module.exports = {
     if (window.location.href === 'http://tortasmundo-kitchen-web-ui-129681185.us-east-2.elb.amazonaws.com/') {
       return 'http://tortasmundo-kitchen-api-1604690468.us-east-2.elb.amazonaws.com'
     }
+    if (window.location.href === 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:3000/') {
+      return 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:4000'
+    }
     return  'http://localhost:4000'
   },
   mocks: {
