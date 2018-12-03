2b94daa6c2fe8a1b0a1fa4d824f17ea357c3a55e
Alonso A. Ortega <alayor3@gmail.com>
2018-12-03 14:34:44 -0700

    Change ports.

--- src/config.js
+++ src/config.js
@@ -6,10 +6,10 @@ module.exports = {
     if (window.location.href === 'http://tortasmundo-kitchen-web-ui-129681185.us-east-2.elb.amazonaws.com/') {
       return 'http://tortasmundo-kitchen-api-1604690468.us-east-2.elb.amazonaws.com'
     }
-    if (window.location.href === 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:3000/') {
+    if (window.location.href === 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:6000/') {
       return 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:5000'
     }
-    return  'http://localhost:4000'
+    return  'http://localhost:5000'
   },
   mocks: {
     store_location: {
